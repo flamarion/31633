905fb32378be2dafe1f9e844abcf478641066108
Flamarion Jorge <flamarion@hashicorp.com>
2020-08-05 11:29:17 +0200

    Test

--- main.tf
+++ main.tf
@@ -4,100 +4,162 @@ terraform {
 
 provider "google" {
   credentials = file("~/.config/gcloud/key.json")
-  project     = "flamarion"
-  region      = "europe-west4"
-  zone        = "europe-west4-a"
   version     = "~> 3.32"
 }
 
+provider "google-beta" {
+  credentials = file("~/.config/gcloud/key.json")
+  version     = "~> 3.32"
+}
+
+resource "random_id" "name" {
+  byte_length = 2
+}
+
+# ------------------------------------------------------------------------------
+# CREATE THE MASTER INSTANCE
+#
+# NOTE: We have multiple google_sql_database_instance resources, based on
+# HA and replication configuration options.
+# ------------------------------------------------------------------------------
+
 resource "google_sql_database_instance" "master" {
-  name             = "master"
+
+  provider         = google-beta
+  name             = "master-${random_id.name.hex}"
+  project          = "flamarion"
+  region           = "europe-west4"
   database_version = "POSTGRES_11"
 
   settings {
-    tier = "db-f1-micro"
+    tier              = "db-f1-micro"
+    activation_policy = "ALWAYS"
+    availability_type = "REGIONAL"
+    disk_autoresize   = true
+    disk_size         = 10
+    disk_type         = "PD_SSD"
+    pricing_plan      = "PER_USE"
+
     ip_configuration {
       ipv4_enabled = true
+      authorized_networks {
+        name  = "all"
+        value = "0.0.0.0/0"
+      }
     }
+
+    location_preference {
+      zone = "europe-west4-a"
+    }
+
     backup_configuration {
+      binary_log_enabled = false
       enabled            = true
-      #binary_log_enabled = true
+      start_time         = "11:30"
     }
-    availability_type = "ZONAL"
   }
 }
 
+# ------------------------------------------------------------------------------
+# CREATE A DATABASE
+# ------------------------------------------------------------------------------
+
 resource "google_sql_database" "default" {
   depends_on = [google_sql_database_instance.master]
-  name       = "my_db"
-  instance   = google_sql_database_instance.master.name
+
+  name     = "db-${random_id.name.hex}"
+  project  = "flamarion"
+  instance = google_sql_database_instance.master.name
 }
 
 resource "google_sql_user" "default" {
   depends_on = [google_sql_database.default]
-  name       = "flamarion"
+  project    = "flamarion"
+  name       = "admin"
   instance   = google_sql_database_instance.master.name
-  password   = "SuperS3cret"
+  # Postgres users don't have hosts, so the API will ignore this value which causes Terraform to attempt
+  # to recreate the user each time.
+  # See https://github.com/terraform-providers/terraform-provider-google/issues/1526 for more information.
+  host     = null
+  password = "SuperS3cret"
 }
 
 
-resource "google_sql_database_instance" "failover_replica" {
+# ------------------------------------------------------------------------------
+# CREATE THE READ REPLICAS
+# ------------------------------------------------------------------------------
+
+resource "google_sql_database_instance" "read_replica" {
+
   depends_on = [
     google_sql_database_instance.master,
     google_sql_database.default,
     google_sql_user.default,
   ]
 
-  name             = "master-failover"
+  provider         = google-beta
+  name             = "replica-${random_id.name.hex}"
+  project          = "flamarion"
+  region           = "europe-west4"
   database_version = "POSTGRES_11"
 
   # The name of the instance that will act as the master in the replication setup.
   master_instance_name = google_sql_database_instance.master.name
 
   replica_configuration {
-    # Specifies that the replica is the failover target.
-    failover_target = true
+    # Specifies that the replica is not the failover target.
+    failover_target = false
   }
+
   settings {
-    tier = "db-f1-micro"
+    tier              = "db-f1-micro"
+    activation_policy = "ALWAYS"
+    availability_type = "REGIONAL"
+    disk_autoresize   = true
+    disk_size         = 10
+    disk_type         = "PD_SSD"
+    pricing_plan      = "PER_USE"
+
     ip_configuration {
       ipv4_enabled = true
+      authorized_networks {
+        name  = "all"
+        value = "0.0.0.0/0"
+      }
     }
+
+    location_preference {
+      zone = "europe-west4-b"
+    }
+
     backup_configuration {
+      binary_log_enabled = false
       enabled            = true
-      #binary_log_enabled = true
     }
-    availability_type = "ZONAL"
   }
 }
 
 
-output "master_instance_name" {
-  description = "The name of the master database instance"
-  value       = google_sql_database_instance.master.name
+# ------------------------------------------------------------------------------
+# CREATE A TEMPLATE FILE TO SIGNAL ALL RESOURCES HAVE BEEN CREATED
+# ------------------------------------------------------------------------------
+
+data "template_file" "complete" {
+  depends_on = [
+    google_sql_database_instance.master,
+    google_sql_database_instance.read_replica,
+    google_sql_database.default,
+    google_sql_user.default,
+  ]
+
+  template = true
 }
 
-output "master_public_ip_address" {
-  description = "The public IPv4 address of the master instance."
-  value       = google_sql_database_instance.master.public_ip_address
+
+output "master" {
+  value = google_sql_database_instance.master
 }
 
-output "db" {
-  description = "Self link to the default database"
-  value       = google_sql_database.default.self_link
-}
-
-output "db_name" {
-  description = "Name of the default database"
-  value       = google_sql_database.default.name
-}
-
-output "failover_instance_name" {
-  description = "The name of the failover database instance"
-  value       = join("", google_sql_database_instance.failover_replica.*.name)
-}
-
-output "failover_public_ip_address" {
-  description = "The public IPv4 address of the failover instance."
-  value       = join("", google_sql_database_instance.failover_replica.*.public_ip_address)
+output "read_reaplica" {
+  value = google_sql_database_instance.read_replica
 }
