13d34ec1a45629d39d06e2c15c8f6fbbcfa9824f
Flamarion Jorge <flamarion@hashicorp.com>
2020-08-06 13:44:50 +0200

    Fixing wrong reference

--- main.tf
+++ main.tf
@@ -142,7 +142,7 @@ resource "google_sql_user" "default" {
 data "template_file" "complete" {
   depends_on = [
     google_sql_database_instance.master,
-    google_sql_database_instance.read_replica,
+    # google_sql_database_instance.read_replica,
     google_sql_database.default,
     google_sql_user.default,
   ]
